30959d37166227d7fe08dcf87ef5b022669f072e
Tom Dyas <tom@shoalsoftware.com>
2025-08-13 14:17:06 -0400

    bump version number

--- src/python/shoalsoft/pants_opentelemetry_plugin/opentelemetry_integration_test.py
+++ src/python/shoalsoft/pants_opentelemetry_plugin/opentelemetry_integration_test.py
@@ -254,7 +254,7 @@ def do_test_of_json_file_exporter(
         )
 
 
-@pytest.mark.parametrize("pants_version_str", ["2.28.0a0", "2.27.0", "2.26.2", "2.25.3"])
+@pytest.mark.parametrize("pants_version_str", ["2.28.0rc0", "2.27.0", "2.26.2", "2.25.3"])
 def test_opentelemetry_integration(subtests, pants_version_str: str) -> None:
     pants_version = Version(pants_version_str)
     pants_major_minor = f"{pants_version.major}.{pants_version.minor}"
